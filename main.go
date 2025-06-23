e6f54b636bdc0ab7c8614f3ddbeb96d7a13a9723
Kaden <kadenw258@gmail.com>
2025-06-23 14:06:41 -0500

    Interface sheet
    A interface sheet that will later container the commands, setting, help etc.

--- main.go
+++ main.go
@@ -1,27 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"modules/internals/mainFunc"
-
-	bubbletea "github.com/charmbracelet/bubbletea"
+	"modules/internals/viewCmd"
 )
-
-
-
-
-
-
 func main() {
-	    _ = bubbletea.NewProgram 
-	cmdMap, err := mainFunc.ParseCommandsFile("commands.txt")
-	args := mainFunc.OsArgs()
+	viewCmd.Main()
+	// cmdMap, err := mainFunc.ParseCommandsFile("commands.txt")
+	// args := mainFunc.OsArgs()
 
-	if (err != nil) || (len(cmdMap) == 0) {
-		fmt.Println("Error reading commands file or no commands found:", err)
-		return
-	}
-	mainFunc.RunCommands(args, cmdMap)
-	fmt.Println(cmdMap)
+	// if (err != nil) || (len(cmdMap) == 0) {
+	// 	fmt.Println("Error reading commands file or no commands found:", err)
+	// 	return
+	// }
+	// mainFunc.RunCommands(args, cmdMap)
+	// fmt.Println(cmdMap)
 }
 
